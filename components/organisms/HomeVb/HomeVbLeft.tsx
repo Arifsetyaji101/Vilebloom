54e76855f302dc4ad103d7fb8ce05e79347ccc3b
Arifsetyaji101 <arifsetyaji101@gmail.com>
2023-01-26 08:51:54 +0700

    try fix font

--- components/organisms/HomeVb/HomeVbLeft.tsx
+++ components/organisms/HomeVb/HomeVbLeft.tsx
@@ -1,13 +1,22 @@
+import localFont from "@next/font/local";
+
+// Font files can be colocated inside of `pages`
+const MeltowSan300 = localFont({
+  src: "../../../public/meltow-font/MeltowSan300-Rust.woff2",
+});
+
 export default function HomeVbLeft() {
   return (
     <>
       <div className="col-lg-5 my-auto">
         <div className="home-vb-left">
-          <h1 className="meltow-300 display-xl gray-vb">
-            Hi!,
-            <br />
-            I'am <span className="pink-vb">Vilebloom</span>
-          </h1>
+          <div className={MeltowSan300.className}>
+            <h1 className="display-xl gray-vb">
+              Hi!,
+              <br />
+              I'am <span className="pink-vb">Vilebloom</span>
+            </h1>
+          </div>
           <h5 className="gray-vb display-base meltow-200">
             a Mechanical Keyboard Keycaps
           </h5>
