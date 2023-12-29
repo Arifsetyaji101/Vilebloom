ee588dc001f867b041281e298c515aa0c261347c
Arif Setyaji <83483824+Arifsetyaji101@users.noreply.github.com>
2023-12-29 14:38:49 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -71,6 +71,18 @@ export default function App({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         ></Script> */}
       </Head>
+       {/*
+        <!-- Google tag (gtag.js) -->
+       */}
+     
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-W43P8VMD53"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-W43P8VMD53');
+</script>
       <Component {...pageProps} />
     </>
   );
